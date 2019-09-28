c6f063ba5cbf07a0b79c1e7527f9102ca5bb650c
jesusalatorre <chuyalatorre@gmail.com>
2019-09-28 16:11:41 -0500

    line minimize tramite

--- front-end/src/views/Tramite.js
+++ front-end/src/views/Tramite.js
@@ -101,19 +101,9 @@ export default class Tramite extends Component {
                     <Col span={12}><Statistic title="Estatus" groupSeparator={""} value={this.state.status} /></Col>
                 </Row>
                 <h2 style={{marginBottom:50}}>{this.state.proceso}</h2>
-                <MediaQuery query="(min-device-width: 1224px)">
-                    <Steps labelPlacement={'vertical'} current={this.state.n_paso} style={{marginBottom:50}}>
-                        {this.state.pasos.map(value => {return (<Steps.Step title={value.nombre_mostrar} />)})}
-                    </Steps>
-                </MediaQuery>
-                <MediaQuery query="(max-device-width: 1223px)">
-                    <Steps direction="vertical" labelPlacement={'vertical'} current={this.state.n_paso} style={{marginBottom:50}}>
-                        {this.state.pasos.map(value => {return (<Steps.Step title={value.nombre_mostrar} />)})}
-                    </Steps>
-                </MediaQuery>
-                {this.state.n_paso==this.state.pasos.length  && !localStorage.getItem("esAdmin")  ?  <Row style={{textAlign:'center', }} gutter={8}>
-                        <h2><a href={"https://forms.gle/GzcmC4f9cmFKS2ee9"} target={"_blank"}>Evalúa los trámites escolares</a></h2>
-                </Row> : <div></div>}
+                <MediaQuery query="(min-device-width: 1224px)"><Steps labelPlacement={'vertical'} current={this.state.n_paso} style={{marginBottom:50}}>{this.state.pasos.map(value => {return (<Steps.Step title={value.nombre_mostrar} />)})}</Steps></MediaQuery>
+                <MediaQuery query="(max-device-width: 1223px)"><Steps direction="vertical" labelPlacement={'vertical'} current={this.state.n_paso} style={{marginBottom:50}}>{this.state.pasos.map(value => {return (<Steps.Step title={value.nombre_mostrar} />)})}</Steps></MediaQuery>
+                {this.state.n_paso==this.state.pasos.length  && !localStorage.getItem("esAdmin")  ?  <Row style={{textAlign:'center', }} gutter={8}><h2><a href={"https://forms.gle/GzcmC4f9cmFKS2ee9"} target={"_blank"}>Evalúa los trámites escolares</a></h2></Row> : <div></div>}
                 <Row gutter={8}>
                     <Col span={12}><Statistic title="Ticket" groupSeparator={""} value={this.state.ticket} prefix={'#'} /></Col>
                     <Col span={12}><Row gutter={8} style={{textAlign:'center'}}>
