a3f1e6e382d7be74033197856de1f11f7b48abcd
jesusalatorre <chuyalatorre@gmail.com>
2019-09-28 13:22:38 -0500

    line minimize formatocarta

--- front-end/src/views/FormatoCartas.js
+++ front-end/src/views/FormatoCartas.js
@@ -71,20 +71,13 @@ export default class FormatoCartas extends Component {
                     <Panel header="Instrucciones" key="1"> <p>{"Si quiere saber mas sobre como crear una carta presione este link copie este link en su navegador : https://docs.google.com/document/d/15ReAuFoavQNVQSqVvKKkB6zbbe79Tt57OqfHdfQ5eeQ/edit?usp=sharing" }</p> </Panel>
                 </Collapse>
                 <DataTable loading={this.state.loading} data={this.state.data} deleteFunc={this.deleteFiles} rowSelection={permitirBorrar}
-                columns={[{
-                    title: 'Nombre del documento',
-                    key: 'nombre_carta', },
-                 {  title: 'Usuario',
-                    key: 'nombre', },
-                {   title: 'Fecha',
-                    key: 'fecha_creacion', },
-                {   title: 'Descripcion',
-                    key: 'descripcion', }]}/>
-                <Modal
-                    width={1300}
-                    title={this.state.record.nombre}
-                    visible={this.state.visible}
-                    onCancel={() => {this.setState({visible:false})}}
+                columns={[
+                    {title: 'Nombre del documento', key: 'nombre_carta', },
+                    {title: 'Usuario', key: 'nombre', },
+                    {title: 'Fecha', key: 'fecha_creacion', },
+                    {title: 'Descripcion', key: 'descripcion', }
+                ]}/>
+                <Modal width={1300} title={this.state.record.nombre} visible={this.state.visible} onCancel={() => {this.setState({visible:false})}}
                     footer={[
                         <Button key="back" onClick={() => {this.setState({visible:false})}}>Cancelar</Button>,
                         <Button key="submit" type="primary"  onClick={() => {this.setState({visible:false})}}> OK </Button>,]}>
