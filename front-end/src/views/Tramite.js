f2af797b4d34d7bad25d86f587bed4ea29024b84
jesusalatorre <chuyalatorre@gmail.com>
2019-09-28 14:27:04 -0500

    line minimize tramite

--- front-end/src/views/Tramite.js
+++ front-end/src/views/Tramite.js
@@ -46,13 +46,8 @@ export default class Tramite extends Component {
             service:'get_pasos_tramites/',
             params:{id:id},
             method:'post',
-            success:(response) => {
-                this.setState({ pasos: response, loading:false, status:this.state.n_paso==response.length ?
-                        "Terminado" : this.state.n_paso == 0 ? "Iniciado":"En proceso" });
-            },
-            error:(response) => {
-                this.setState({loading:false});
-            }
+            success:(response) => {this.setState({ pasos: response, loading:false, status:this.state.n_paso==response.length ? "Terminado" : this.state.n_paso == 0 ? "Iniciado":"En proceso" });},
+            error:(response) => {this.setState({loading:false});}
         })
     }
 
@@ -70,9 +65,7 @@ export default class Tramite extends Component {
                 n_paso:response[0].numero_paso_actual });
                 this.getPasos(response[0].proceso_id)
                 },
-            error:(response) => {
-                this.setState({loading:false});
-            }
+            error:(response) => {this.setState({loading:false});}
         })
     };
 
@@ -100,53 +93,32 @@ export default class Tramite extends Component {
     render() {
         return (
             <Spin spinning={this.state.loading}><div>
-                <h2 style={{float:'right'}}>{ localStorage.getItem("esAdmin") ? 
-                    <Statistic title="Matrícula" groupSeparator={""} value={(localStorage.getItem("matAlumno") || " ").toLocaleUpperCase()} />
-                 : "" }</h2>
+                <h2 style={{float:'right'}}>{ localStorage.getItem("esAdmin") ? <Statistic title="Matrícula" groupSeparator={""} value={(localStorage.getItem("matAlumno") || " ").toLocaleUpperCase()} />: "" }</h2>
                 <h1>{ localStorage.getItem("esAdmin") ? "Trámite del alumno" : "Mi trámite" }</h1>
                 <Divider/>
                 <Row style={{float:'right'}} gutter={8}>
-                    <Col span={12}>
-                        <Statistic title="Días transcurridos" groupSeparator={""} value={moment().diff(moment(this.state.fecha1),'days')} />
-                    </Col>
-                    <Col span={12}>
-                        <Statistic title="Estatus" groupSeparator={""} value={this.state.status} />
-                    </Col>
+                    <Col span={12}><Statistic title="Días transcurridos" groupSeparator={""} value={moment().diff(moment(this.state.fecha1),'days')} /></Col>
+                    <Col span={12}><Statistic title="Estatus" groupSeparator={""} value={this.state.status} /></Col>
                 </Row>
                 <h2 style={{marginBottom:50}}>{this.state.proceso}</h2>
                 <MediaQuery query="(min-device-width: 1224px)">
                     <Steps labelPlacement={'vertical'} current={this.state.n_paso} style={{marginBottom:50}}>
-                        {
-                            this.state.pasos.map(value => {
-                                return (<Steps.Step title={value.nombre_mostrar} />)
-                            })
-                        }
+                        {this.state.pasos.map(value => {return (<Steps.Step title={value.nombre_mostrar} />)})}
                     </Steps>
                 </MediaQuery>
                 <MediaQuery query="(max-device-width: 1223px)">
                     <Steps direction="vertical" labelPlacement={'vertical'} current={this.state.n_paso} style={{marginBottom:50}}>
-                        {
-                            this.state.pasos.map(value => {
-                                return (<Steps.Step title={value.nombre_mostrar} />)
-                            })
-                        }
+                        {this.state.pasos.map(value => {return (<Steps.Step title={value.nombre_mostrar} />)})}
                     </Steps>
                 </MediaQuery>
                 {this.state.n_paso==this.state.pasos.length  && !localStorage.getItem("esAdmin")  ?  <Row style={{textAlign:'center', }} gutter={8}>
                         <h2><a href={"https://forms.gle/GzcmC4f9cmFKS2ee9"} target={"_blank"}>Evalúa los trámites escolares</a></h2>
                 </Row> : <div></div>}
                 <Row gutter={8}>
-                    <Col span={12}>
-                        <Statistic title="Ticket" groupSeparator={""} value={this.state.ticket} prefix={'#'} />
-                    </Col>
-                    <Col span={12}>
-                        <Row gutter={8} style={{textAlign:'center'}}>
-                            <Col span={12}>
-                                <Statistic title="Fecha de inicio" value={moment.utc(this.state.fecha1).format("DD/MM/YYYY")} prefix={<Icon type="calendar" />} />
-                            </Col>
-                            <Col span={12}>
-                                <Statistic title="Fecha de última actualización" value={moment.utc(this.state.fecha2).format("DD/MM/YYYY")} prefix={<Icon type="calendar" />}/>
-                            </Col>
+                    <Col span={12}><Statistic title="Ticket" groupSeparator={""} value={this.state.ticket} prefix={'#'} /></Col>
+                    <Col span={12}><Row gutter={8} style={{textAlign:'center'}}>
+                        <Col span={12}><Statistic title="Fecha de inicio" value={moment.utc(this.state.fecha1).format("DD/MM/YYYY")} prefix={<Icon type="calendar" />} /></Col>
+                        <Col span={12}><Statistic title="Fecha de última actualización" value={moment.utc(this.state.fecha2).format("DD/MM/YYYY")} prefix={<Icon type="calendar" />}/></Col>
                         </Row>
                     </Col>
                 </Row>
