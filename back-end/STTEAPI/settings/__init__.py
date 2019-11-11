3f3a2e82a9ef13a733d78377ecb94348f7454e8f
Enrique Barragan <ebarragan1997@gmail.com>
2019-11-11 15:18:56 -0600

    fix bug on init.py

--- back-end/STTEAPI/settings/__init__.py
+++ back-end/STTEAPI/settings/__init__.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import os
-import environ
+from os import environ
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
